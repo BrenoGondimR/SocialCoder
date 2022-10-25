69ae8be460304ec6789f8d5617280fcf3ff6928b
Breno Gondim <breno_gondim@hotmail.com>
2022-10-25 01:16:35 -0300

    Finalizando

--- Scripts/main.js
+++ Scripts/main.js
@@ -2,3 +2,9 @@ function irParaFeed(){
     window.location.href = ("http://127.0.0.1:5501/index.html");
     console.log("Deu certo")
 }
+
+function irParaPubli(){
+    window.location.href = ("http://127.0.0.1:5501/post.html");
+    console.log("Deu certo")
+}
+
